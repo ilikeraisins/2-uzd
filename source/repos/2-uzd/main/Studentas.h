61d89bfefcf3deffc321e39093a472d097e8e8da
unknown <monemuznikaite@gmail.com>
2025-11-13 18:14:11 +0200

    Studentas.h

---
+++ source/repos/2-uzd/main/Studentas.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <vector>
+#include <list>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::setw;
+using std::left;
+using std::right;
+using std::list;
+using std::vector;
